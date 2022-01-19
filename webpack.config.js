12104c928b51039a574f3acd9f87374ba2a00f4f
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-01-19 22:23:40 +0700

    1 step

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   entry: './src/index.js',
-	output: {
+  output: {
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {

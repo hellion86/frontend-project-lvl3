e4998fdfbc3de95123eb5231a9ae8ad2e96fc85d
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-01-22 08:12:14 +0700

    1 step

--- webpack.config.js
+++ webpack.config.js
@@ -18,9 +18,6 @@ const config = {
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [

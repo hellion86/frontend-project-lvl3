c0b1e435063acb1ae11a94f59cf5d33ea1d52fbb
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-01-25 06:50:11 +0700

    step 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import keyBy from 'lodash/keyBy.js';
 import * as yup from 'yup';
 import onChange from 'on-change';
 
@@ -8,128 +7,35 @@ import onChange from 'on-change';
 
 const state = {
 	form: {
-		feeds: '',
+		url: '',
 	},
 	valid: true,
-	errors: [],
 };
-
-
-
-// const schema = yup.object().shape({
-//   //feed: yup.string().trim().required()
-// 	feeds: yup.string().url(),
-//   // email: yup.string().required().email(),
-//   // password: yup.string().required().min(6),
-//   // passwordConfirmation: yup.string()
-//   //  .required('password confirmation is a required field')
-//   //  .oneOf(
-//   //    [yup.ref('password'), null],
-//   //    'password confirmation does not match to password',
-//   //  ),
-// });
-
-
-const regMatch = /^((http|https):\/\/)?(www.)?(?!.*(http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+(\/)?.([\w\?[a-zA-Z-_%\/@?]+)*([^\/\w\?[a-zA-Z0-9_-]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/;
 const schema = yup.object({ url: yup.string().url() });
-//matches(regMatch, 'Website should be a valid URL')
-//const schema = yup.object({
-// console.log(schema.isValidSync(12345)) // (true) This is valid.
-// console.log(schema.isValidSync(00123)) /
+const elements = {
+	mainForm: document.querySelector('form'),
+	dangerZone: document.querySelector('.feedback'),
+	mainFormUrlInput: document.querySelector('#url-input'),
+};
 
 // получить инпут формы
 // повешать обработчик, который вносит данные в стейт при измении формы
-
-
 // этот вотчер следит за изменением стейта и будет проверять валидность ссылки
 const watchedState = onChange(state, () => {
-	console.log(state);
-	// const resultValidation = validate(state.registrationForm.data);
-	// allInputs.forEach((input) => {
-	// 	const errorDiv = input.closest('.form-group').querySelector('#errorPlace');
-	// 	if (has(resultValidation, input.name)) {
-	// 		input.classList.add('is-invalid');
-	// 		errorDiv.classList.add('invalid-feedback');
-	// 		errorDiv.textContent = resultValidation[input.name].message;
-	// 	} else {
-	// 		errorDiv.textContent = '';
-	// 		errorDiv.classList.remove('invalid-feedback');
-	// 		input.classList.remove('is-invalid');
-	// 	}
-	// });
-	// const submitFormButton = document.querySelector('.btn-primary');
-	// submitFormButton.disabled = !isEmpty(resultValidation);
+	schema.validate(state.form).then(() => {
+		state.valid = true;
+		elements.dangerZone.textContent = '';
+		elements.mainFormUrlInput.classList.remove('is-invalid');
+	}).catch(() => {
+		elements.dangerZone.textContent = 'Ссылка должна быть валидным URL';
+		state.valid = false;
+		elements.mainFormUrlInput.classList.add('is-invalid');
+	});
 });
-// const validate = (field) => {
-//   // try {
-//   //   schema.validateSync(fields, { abortEarly: false });
-//   //   return {};
-//   // } catch (e) {
-//   //   return keyBy(e.inner, 'path');
-//   // }
 
-// 	schema.validate(field, { abortEarly: false })
-// 	.then((result) => {
-// 		console.log('ok');
-// 		console.log(result);
-		
-// 		watchedState.feeds = result;
-// 		watchedState.form.valid = true;
-// 	})
-// 	.catch((err) => {
-// 		console.log(keyBy(err.inner, 'path'));
-// 		watchedState.feeds = '';
-// 		watchedState.form.valid = false;
-// 	});
-// };
-
-
-const mainForm = document.querySelector('form');
-mainForm.addEventListener('submit', (e) => {
+elements.mainForm.addEventListener('submit', (e) => {
 	e.preventDefault();
-	// console.log(e.target);
-	// console.log(e.target);
 	const formData = new FormData(e.target);
 	const urlFromForm = formData.get('url');
-	state.form.feeds = urlFromForm;
-	//validate(urlFromForm);
-	schema.validate(state.form).then(console.log).catch((err) => console.log(err));
-	//console.log(formData.get('url'));
-	//state.feeds = urlFromForm;
-	//console.log(state);
-	//fsp.readFile(src, 'utf-8').then((content) => console.log(content));
-	//const resultValidation = schema.isValidSync(state.feeds);
-	//console.log(resultValidation);
-	//watchedState.registrationForm.data[e.target.name] = e.target.value;
-	
-
-
-
-
-
-	
-
-
-
-	
-	// schema.isValid(urlFromForm)
-	// 	.then((result) => {
-	// 		//watchedState.form.errors.push(!result);
-	// 		watchedState.feeds = urlFromForm;
-	// 		watchedState.form.valid = true;
-	// 	})
-	// 	.catch(() => {
-	// 		watchedState.form.valid = false;
-	// 	});
-	
-
-	// 
-	// state.registrationForm.state = false;
-	// axios.post(routes.usersPath(), state.registrationForm.data)
-	// 	.then((responce) => {
-	// 		document.body.innerHTML = '<div data-container="sign-up">User Created!</div>';
-	// 	})
-	// 	.catch((error) => {
-	// 		console.log(error);
-	// 	});
+	watchedState.form.url = urlFromForm;
 });

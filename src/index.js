f73c832435be217ea4051158f758bb0e034f5262
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-01-25 07:26:29 +0700

    step 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as yup from 'yup';
 import onChange from 'on-change';
-
+// import { watchedState } from './view.js';
 // добавить начальное состояние
 // форма пустая с фокусом ввода в неё
 
 const state = {
-	form: {
+	urlForm: {
 		url: '',
+		valid: true,
 	},
-	valid: true,
 };
 const schema = yup.object({ url: yup.string().url() });
 const elements = {
@@ -22,20 +22,20 @@ const elements = {
 // повешать обработчик, который вносит данные в стейт при измении формы
 // этот вотчер следит за изменением стейта и будет проверять валидность ссылки
 const watchedState = onChange(state, () => {
-	schema.validate(state.form).then(() => {
-		state.valid = true;
+	schema.validate(state.urlForm).then(() => {
+		state.urlForm.valid = true;
 		elements.dangerZone.textContent = '';
 		elements.mainFormUrlInput.classList.remove('is-invalid');
 	}).catch(() => {
 		elements.dangerZone.textContent = 'Ссылка должна быть валидным URL';
-		state.valid = false;
+		state.urlForm.valid = false;
 		elements.mainFormUrlInput.classList.add('is-invalid');
 	});
 });
-
+// const mainForm = document.querySelector('form');
 elements.mainForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const formData = new FormData(e.target);
-	const urlFromForm = formData.get('url');
-	watchedState.form.url = urlFromForm;
+	const getUrl = formData.get('url');
+	watchedState.urlForm.url = getUrl;
 });

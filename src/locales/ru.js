0ba64b0bf239990f9409990680ff60605bfda3a3
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-02-06 18:22:15 +0700

    step 7

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -2,7 +2,7 @@ export default {
 	translation: {
 		urlError: 'Ссылка должна быть валидным URL',
 		urlExist: 'RSS уже существует',
-		urlLoadSuccess: 'Rss успешно загружен',
+		urlLoadSuccess: 'RSS успешно загружен',
 		badRss: 'Ресурс не содержит валидный RSS',
 		netWorkError: 'Ошибка сети',
 	},

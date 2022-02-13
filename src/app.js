13ecfce38ddb5a812143d8300eb5b1905684c02f
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-02-13 22:05:02 +0700

    check 1

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const app = (i18) => {
     state.urlForm.url = formData.get('url');
     state.urlForm.status = 'loadUrl';
     validateUrl(state.urlForm)
-      .then((data) => loadUrl(data.url))
+      .then((data) => loadUrl(new URL(data.url)))
       .then((rss) => {
         const [feed, posts] = parserUrl(rss, state.urlForm.url);
         state.urlForm.loadedUrl.push(state.urlForm.url);

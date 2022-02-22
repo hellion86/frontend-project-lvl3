a3eab28528f0db95c8430b71d95c69fdbbdfcfad
Vyacheslav Mezhurevskiy <VMezhurevsky@gmail.com>
2022-02-22 10:53:55 +0700

    check 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,11 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    open: true,
-    host: 'localhost',
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './index.html',
@@ -22,8 +14,14 @@ const config = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
